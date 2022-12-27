ed8f1906559c1a6ddd6f71fe28584b1047d03493
conorbailey90 <conbailey90@gmail.com>
2022-12-27 15:03:18 +0000

    Google Analytics added.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-LJ7JWR160N"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LJ7JWR160N', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
